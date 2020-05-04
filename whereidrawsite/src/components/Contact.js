362bf63dd6da9719de377b0288094aca83bd17a2
connie <conniedixon106@gmail.com>
2020-05-04 15:54:52 +0100

    Added contact functionality

--- whereidrawsite/src/components/Contact.js
+++ whereidrawsite/src/components/Contact.js
@@ -1,9 +1,42 @@
 /** @format */
 
-import React from "react";
+import React, { Component } from "react";
 
-const Contact = () => {
-  return <div>Contact</div>;
-};
+class Contact extends Component {
+  state = {
+    sent: false,
+  };
+
+  toggleSent = () => {
+    this.setState({ sent: true });
+  };
+
+  render() {
+    if (this.state.sent)
+      return (
+        <>
+          <h3>Get in Touch</h3>
+          <p>Thanks for your message, I'll be in touch soon!</p>
+        </>
+      );
+    return (
+      <>
+        <h3>Get in Touch</h3>
+        <p>Let's chat! Email whereidraw@gmail.com or fill in the form below.</p>
+        <form>
+          <input type='text' id='name' placeholder='Name' />
+          <br />
+          <input type='text' id='email' placeholder='Email' />
+          <br />
+          <textarea id='message' placeholder='Type your message here...' />
+          <br />
+          <button type='submit' onClick={this.toggleSent}>
+            Submit
+          </button>
+        </form>
+      </>
+    );
+  }
+}
 
 export default Contact;
