9d99c64cdf1e150019a1aed4a99329ccabeb38f1
connie <conniedixon106@gmail.com>
2020-05-04 15:56:59 +0100

    Added about infomration

--- whereidrawsite/src/components/About.js
+++ whereidrawsite/src/components/About.js
@@ -3,7 +3,21 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <h4>Hello,</h4>
+      <p>
+        my name is Ricky and I'm a freelance Illustrator currently living and
+        working in Leeds. I love creating artwork that clings to reality by its
+        fingertips to tell a story. And I really enjoy the challenge of
+        combining elements that you would never usually see together in real
+        life to achieve thought provoking new concepts. When I'm not found with
+        a pen in hand you can find me out in nature walking my dog, sat in the
+        pub with a beer and some friends or occasionally battling imaginary
+        goblins in a dungeons and dragons campaign.
+      </p>
+    </>
+  );
 };
 
 export default About;
