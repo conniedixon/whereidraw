02a00397ff010bbcc828f5cfd0b5e0cb24cb098f
connie <conniedixon106@gmail.com>
2020-05-05 15:09:44 +0100

    Added css for home title

--- whereidrawsite/src/components/Home.js
+++ whereidrawsite/src/components/Home.js
@@ -1,9 +1,17 @@
 /** @format */
 
 import React from "react";
+import { Link } from "@reach/router";
 
 const Home = () => {
-  return <div>HomePage</div>;
+  return (
+    <span className='home'>
+      <Link to='/portfolio'>
+        <h1 className='homeTitle'>Ricky {<div />}BUTCHER</h1>
+      </Link>
+      <h2 className='homeSubtitle'>illustrator</h2>
+    </span>
+  );
 };
 
 export default Home;
