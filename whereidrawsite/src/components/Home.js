54341e14c1740cd41513dba5329e46793d1d8f73
connie <conniedixon106@gmail.com>
2020-05-15 18:47:08 +0100

    Added background animations

--- whereidrawsite/src/components/Home.js
+++ whereidrawsite/src/components/Home.js
@@ -6,9 +6,18 @@ import { Link } from "@reach/router";
 const Home = () => {
   return (
     <span className='home'>
-      <Link to='/portfolio'>
-        <h1 className='homeTitle'>Ricky {<div />}BUTCHER</h1>
-      </Link>
+      <span className='rectangle'></span>
+      <span className='square'></span>
+
+      <div className={"canvas"}>
+        <Link to='/portfolio'>
+          <span className={"ball"}></span>
+        </Link>
+      </div>
+      <p className={"click-here"}>CATCH⇨</p>
+      <h1 className='homeTitle'>Ricky {<div />}BUTCHER</h1>
+      <span className='bigcircle'></span>
+
       <p className={"p"}>
         Hello, my name is Ricky and I'm a freelance illustrator living in Leeds,
         UK. I create fantasy and surrealist artwork, combining elements you
