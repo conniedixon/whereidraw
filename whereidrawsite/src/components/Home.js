0c820abb22e3f230085a5c3394b7024744556df7
connie <conniedixon106@gmail.com>
2020-05-15 10:40:08 +0100

    Made home page title responsive

--- whereidrawsite/src/components/Home.js
+++ whereidrawsite/src/components/Home.js
@@ -9,14 +9,13 @@ const Home = () => {
       <Link to='/portfolio'>
         <h1 className='homeTitle'>Ricky {<div />}BUTCHER</h1>
       </Link>
-      <h2 className='homeSubtitle'>illustrator</h2>
-      {/* <p>
+
+      <p>
         Hello, my name is Ricky and I'm a freelance illustrator living in Leeds,
-        UK. I create fantasy artwork, combining elements you would never see
-        together in real life to achieve thought-provoking concepts for
-        editorial, albums and books. Get in touch if you have an idea I can help
-        you visualise.
-      </p> */}
+        UK. I create fantasy and surrealist artwork, combining elements you
+        would never see together in real life to achieve thought-provoking
+        concepts for editorial, albums and books.
+      </p>
     </span>
   );
 };
